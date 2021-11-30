7517e26ca02edc1a9b79bb65782c4a3079bebb41
unaxiee <xieyongyu16@foxmail.com>
2021-11-30 11:40:05 -0500

    try pydrill

--- application.py
+++ application.py
@@ -10,68 +10,81 @@ application = Flask(__name__)
 def home_page():
     try:
         if (request.method == 'POST'): 
-
             database = request.form['database']
             dataset = request.form['dataset']
-            
-            if (database == 'mysql'):
-                if (dataset == 'adnimerge'):
-                    connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
-                                                         database='adnimerge',
-                                                         user='mysql2021',
-                                                         password='mysql2021')
-                elif (dataset == 'instacart_normalized'):
-                    connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
-                                                         database='instacart_normalized',
-                                                         user='mysql2021',
-                                                         password='mysql2021')
-                elif (dataset == 'instacart'):
-                    connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
-                                                         database='instacart',
-                                                         user='mysql2021',
-                                                         password='mysql2021')
-            elif (database == 'redshift'):
-                if (dataset == 'adnimerge'):
-                    connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
-                                                  database='adnimerge',
-                                                  user='redshift2021',
-                                                  password='Redshift2021',
-                                                  port="5439")
-                elif (dataset == 'instacart_normalized'):
-                    connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
-                                                  database='instacart_normalized',
-                                                  user='redshift2021',
-                                                  password='Redshift2021',
-                                                  port="5439")
-                elif (dataset == 'instacart'):
-                    connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
-                                                  database='instacart',
-                                                  user='redshift2021',
-                                                  password='Redshift2021',
-                                                  port="5439")
-            cursor = connection.cursor()
-            query = request.form['query']
-            init_time = datetime.datetime.now()
-            cursor.execute(query)
-            end_time = datetime.datetime.now()
+            if (database == 'mongodb'):
+                drill = PyDrill(host='localhost', port=8047)
+                if drill.is_active():
+                    query = 'yes'
+                else:
+                    query = 'no'
+                table_head=[]
+                table_data=[]
+                num=str(1)
+                time=str(1)
+                database=database,
+                dataset=dataset
 
-            if (query[0:6].lower() == 'select' or query[0:4].lower() == 'show' or query[0:8].lower() == 'describe'):
-                table_head = []
-                column_name = cursor.description
-                for i in column_name:
-                    table_head.append(i[0])
-                table_data = cursor.fetchall()
-                num = 'Number of records: ' + str(cursor.rowcount)
             else:
-                connection.commit()
-                table_head = ['Query', 'Execution result']
-                table_data = [[query, 'Finished']]
-                num = 'Number of records: 1'
 
-            cursor.close()
-            connection.close()
-            time = end_time - init_time
-            query = query.replace('\r\n', '?')
+                if (database == 'mysql'):
+                    if (dataset == 'adnimerge'):
+                        connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
+                                                             database='adnimerge',
+                                                             user='mysql2021',
+                                                             password='mysql2021')
+                    elif (dataset == 'instacart_normalized'):
+                        connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
+                                                             database='instacart_normalized',
+                                                             user='mysql2021',
+                                                             password='mysql2021')
+                    elif (dataset == 'instacart'):
+                        connection = mysql.connector.connect(host='mysql2021.c7wtal8gxuuf.us-east-2.rds.amazonaws.com',
+                                                             database='instacart',
+                                                             user='mysql2021',
+                                                             password='mysql2021')
+                elif (database == 'redshift'):
+                    if (dataset == 'adnimerge'):
+                        connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
+                                                      database='adnimerge',
+                                                      user='redshift2021',
+                                                      password='Redshift2021',
+                                                      port="5439")
+                    elif (dataset == 'instacart_normalized'):
+                        connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
+                                                      database='instacart_normalized',
+                                                      user='redshift2021',
+                                                      password='Redshift2021',
+                                                      port="5439")
+                    elif (dataset == 'instacart'):
+                        connection = psycopg2.connect(host='redshift2021.cxixtqv0g2ru.us-east-2.redshift.amazonaws.com',
+                                                      database='instacart',
+                                                      user='redshift2021',
+                                                      password='Redshift2021',
+                                                      port="5439")
+                cursor = connection.cursor()
+                query = request.form['query']
+                init_time = datetime.datetime.now()
+                cursor.execute(query)
+                end_time = datetime.datetime.now()
+
+                if (query[0:6].lower() == 'select' or query[0:4].lower() == 'show' or query[0:8].lower() == 'describe'):
+                    table_head = []
+                    column_name = cursor.description
+                    for i in column_name:
+                        table_head.append(i[0])
+                    table_data = cursor.fetchall()
+                    num = 'Number of records: ' + str(cursor.rowcount)
+                else:
+                    connection.commit()
+                    table_head = ['Query', 'Execution result']
+                    table_data = [[query, 'Finished']]
+                    num = 'Number of records: 1'
+
+                cursor.close()
+                connection.close()
+                time = end_time - init_time
+                query = query.replace('\r\n', '?')
             
             return render_template('index.html', table_head=table_head, table_data=table_data, num=num, time=time, database=database, dataset=dataset, query=query)
                 #return '<h1><br>Executed query: {Done}</br><br>Time Elapsed: {time}</br></h1>'.format(Done=query,time=time_elapsed)
